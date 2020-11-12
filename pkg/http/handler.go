fff68e56510d473d4736a00d2598f096754d4e58
Spencer Small <641091+ssmall@users.noreply.github.com>
2020-11-11 23:01:32 -0800

    Create HTTP handler to extract and re-upload clips (#7)

---
+++ pkg/http/handler.go
@@ -0,0 +1,127 @@
+// Copyright 2020 Spencer Small
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package http contains functionality related to serving http requests
+package http
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/ssmall/nocco-video-extractor/pkg/drive"
+	"github.com/ssmall/nocco-video-extractor/pkg/video"
+)
+
+// ClipExtractionHandler creates a http.HandlerFunc that handles requests to
+// extract video clips from Google Drive files and reupload them to Drive.
+func ClipExtractionHandler(d drive.Client, e video.Extractor) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		var body ExtractionRequest
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		start, err := parseDuration(body.ClipStartTime)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		end, err := parseDuration(body.ClipEndTime)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		filename, contents, err := d.GetFile(r.Context(), body.SourceFileID)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		defer contents.Close()
+
+		f, err := os.Create(path.Join(os.TempDir(), filename))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		defer f.Close()
+		defer os.Remove(f.Name())
+
+		_, err = io.Copy(f, contents)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		transcode, err := e.Clip(r.Context(), f.Name(), start, end)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		defer transcode.Close()
+
+		ext := filepath.Ext(filename)
+		base := strings.TrimSuffix(filename, ext)
+		newFilename := fmt.Sprintf("%s_%s_to_%s%s", base, body.ClipStartTime, body.ClipEndTime, ext)
+
+		log.Printf("Uploading clip as %q", newFilename)
+
+		url, err := d.UploadFile(r.Context(), newFilename, body.DestinationFolderID, transcode)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		resp, err := json.Marshal(&ExtractionResponse{FileURL: url})
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write(resp)
+	}
+}
+
+func parseDuration(timestamp string) (time.Duration, error) {
+	r := regexp.MustCompile(`^(\d{2}):(\d{2}):(\d{2})$`)
+	matches := r.FindStringSubmatch(timestamp)
+	if matches == nil {
+		return 0, fmt.Errorf("%q does not match format HH:MM:SS", timestamp)
+	}
+	return time.ParseDuration(fmt.Sprintf("%sh%sm%ss", matches[1], matches[2], matches[3]))
+}
---
+++ pkg/http/handler_test.go
@@ -0,0 +1,291 @@
+// Copyright 2020 Spencer Small
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package http
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+type closingBuffer struct {
+	b *bytes.Buffer
+}
+
+func (b *closingBuffer) Close() error {
+	return nil
+}
+
+func (b *closingBuffer) Read(p []byte) (n int, err error) {
+	return b.b.Read(p)
+}
+
+type fakeDriveClient struct {
+	// Stub outputs
+	filename       string
+	fileContents   closingBuffer
+	createdFileURL string
+
+	// Stub errors
+	getFileError error
+	uploadError  error
+
+	// Capture inputs
+	getFileID          string
+	uploadFileName     string
+	uploadFileFolder   string
+	uploadFileContents []byte
+}
+
+func (c *fakeDriveClient) GetFile(ctx context.Context, id string) (string, io.ReadCloser, error) {
+	if c.getFileError != nil {
+		return "", nil, c.getFileError
+	}
+	c.getFileID = id
+	return c.filename, &c.fileContents, nil
+}
+
+func (c *fakeDriveClient) UploadFile(ctx context.Context, name, folder string, contents io.Reader) (string, error) {
+	if c.uploadError != nil {
+		return "", c.uploadError
+	}
+	var err error
+	c.uploadFileName = name
+	c.uploadFileFolder = folder
+	c.uploadFileContents, err = ioutil.ReadAll(contents)
+	return c.createdFileURL, err
+}
+
+type fakeExtractor struct {
+	// Stub outputs
+	contents closingBuffer
+
+	// Stub errors
+	err error
+
+	// Capture inputs
+	clipFilename string
+	clipStart    time.Duration
+	clipEnd      time.Duration
+}
+
+func (e *fakeExtractor) Clip(ctx context.Context, filename string, start time.Duration, end time.Duration) (io.ReadCloser, error) {
+	if e.err != nil {
+		return nil, e.err
+	}
+	e.clipFilename = filename
+	e.clipStart = start
+	e.clipEnd = end
+	return &e.contents, nil
+}
+
+func createRequest(t *testing.T, requestJSON string) *http.Request {
+	t.Helper()
+	req, err := http.NewRequest(http.MethodPost, "/extract", bytes.NewBuffer([]byte(requestJSON)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("content-type", "application/json")
+	return req
+}
+
+func TestParseDuration(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected time.Duration
+	}{
+		{
+			input:    "12:34:56",
+			expected: 12*time.Hour + 34*time.Minute + 56*time.Second,
+		},
+		{
+			input:    "00:00:00",
+			expected: 0,
+		},
+		{
+			input:    "00:00:90",
+			expected: 90 * time.Second,
+		},
+	}
+	for _, test := range cases {
+		t.Run(test.input, func(t *testing.T) {
+			actual, err := parseDuration(test.input)
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(test.expected, actual); diff != "" {
+				t.Error("parseDuration output different than expected (+got -want):", diff)
+			}
+		})
+	}
+}
+
+func TestParseDuration_error(t *testing.T) {
+	_, err := parseDuration("asdasd")
+
+	if err == nil {
+		t.Errorf("Expected error")
+	}
+}
+
+func TestHandler_HappyPath(t *testing.T) {
+	drive := &fakeDriveClient{
+		filename:       "originalFile.fileExt",
+		fileContents:   closingBuffer{bytes.NewBufferString("original file contents")},
+		createdFileURL: "https://example.com",
+	}
+	extractor := &fakeExtractor{
+		contents: closingBuffer{bytes.NewBufferString("clip contents")},
+	}
+	handler := ClipExtractionHandler(drive, extractor)
+
+	requestJSON := `{
+		"sourceFileId": "sourceFileId",
+		"clipStartTime": "00:01:23",
+		"clipEndTime": "00:02:34",
+		"destinationFolderId": "destinationFolderId"
+		}`
+
+	req := createRequest(t, requestJSON)
+	rr := httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+
+	if drive.getFileID != "sourceFileId" {
+		t.Errorf("got GetFile(context, %q), want GetFile(context, %q) ", drive.getFileID, "sourceFileId")
+	}
+
+	clipBase := filepath.Base(extractor.clipFilename)
+	expectedStart := 1*time.Minute + 23*time.Second
+	expectedEnd := 2*time.Minute + 34*time.Second
+	if clipBase != drive.filename || extractor.clipStart != expectedStart || extractor.clipEnd != expectedEnd {
+		t.Errorf("got Clip(context, %q, %s, %s), want Clip(context, %q, %s, %s)", clipBase, extractor.clipStart, extractor.clipEnd, drive.filename, expectedStart, expectedEnd)
+	}
+
+	expectedUploadName := "originalFile_00:01:23_to_00:02:34.fileExt"
+	if drive.uploadFileName != expectedUploadName || drive.uploadFileFolder != "destinationFolderId" || cmp.Diff(drive.uploadFileContents, []byte("clip contents")) != "" {
+		t.Errorf("got UploadFile(context, %q, %q, %q), want UploadFile(context, %q, %q, %q)", drive.uploadFileName, drive.uploadFileFolder, drive.uploadFileContents, expectedUploadName, "destinationFolderId", []byte("clip contents"))
+	}
+
+	if diff := cmp.Diff(http.StatusCreated, rr.Code); diff != "" {
+		t.Fatal("Different response code than expected (+got -want):", diff)
+	}
+
+	expected := ExtractionResponse{
+		FileURL: drive.createdFileURL,
+	}
+
+	var actual ExtractionResponse
+	if err := json.Unmarshal(rr.Body.Bytes(), &actual); err != nil {
+		t.Fatalf("Invalid response %q: %v", rr.Body, err)
+	}
+
+	if diff := cmp.Diff(expected, actual); diff != "" {
+		t.Error("Different response than expected (+got -want):", diff)
+	}
+}
+
+func TestHandler_Error(t *testing.T) {
+	validRequest := `{
+		"sourceFileId": "sourceFileId",
+		"clipStartTime": "00:01:23",
+		"clipEndTime": "00:02:34",
+		"destinationFolderId": "destinationFolderId"
+		}`
+
+	cases := []struct {
+		name                 string
+		requestBody          string
+		drive                *fakeDriveClient
+		extractor            *fakeExtractor
+		expectedResponseCode int
+	}{
+		{
+			name:                 "Invalid Request (not a JSON object)",
+			requestBody:          "blah",
+			expectedResponseCode: http.StatusBadRequest,
+		},
+		{
+			name:                 "Invalid Request (invalid start time)",
+			requestBody:          `{"clipStartTime": "blah", "clipEndTime": "00:02:34"}`,
+			expectedResponseCode: http.StatusBadRequest,
+		},
+		{
+			name:                 "Invalid Request (invalid end time)",
+			requestBody:          `{"clipStartTime": "00:02:34", "clipEndTime": "blah"}`,
+			expectedResponseCode: http.StatusBadRequest,
+		},
+		{
+			name:        "Error getting file from drive",
+			requestBody: validRequest,
+			drive: &fakeDriveClient{
+				getFileError: errors.New("expected error"),
+			},
+			expectedResponseCode: http.StatusInternalServerError,
+		},
+		{
+			name:        "Transcoding error",
+			requestBody: validRequest,
+			drive: &fakeDriveClient{
+				filename:     "test file",
+				fileContents: closingBuffer{bytes.NewBufferString("file contents don't matter")},
+			},
+			extractor: &fakeExtractor{
+				err: errors.New("expected error"),
+			},
+			expectedResponseCode: http.StatusInternalServerError,
+		},
+		{
+			name:        "Upload error",
+			requestBody: validRequest,
+			drive: &fakeDriveClient{
+				filename:     "test file",
+				fileContents: closingBuffer{bytes.NewBufferString("file contents don't matter")},
+				uploadError:  errors.New("expected error"),
+			},
+			extractor: &fakeExtractor{
+				contents: closingBuffer{bytes.NewBufferString("transcode contents")},
+			},
+			expectedResponseCode: http.StatusInternalServerError,
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.name, func(t *testing.T) {
+			handler := ClipExtractionHandler(test.drive, test.extractor)
+
+			req := createRequest(t, test.requestBody)
+			rr := httptest.NewRecorder()
+
+			handler.ServeHTTP(rr, req)
+
+			if diff := cmp.Diff(test.expectedResponseCode, rr.Code); diff != "" {
+				t.Fatal("Different response code than expected (+got -want):", diff)
+			}
+		})
+	}
+}
